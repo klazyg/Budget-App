bec641fab80350170648798d6d1d9dac9cb5ca02
klazyg <kzygmunt0@gmail.com>
2023-01-16 17:35:54 +0100

    added form

---
+++ components/Form/Form.tsx
@@ -0,0 +1,15 @@
+// import React, { useState } from "react";
+// import styles from "./Form.module.scss";
+
+
+
+
+// const Form: React.FC = () => {
+//   return (
+//     <form className={styles.position}>
+
+//     </form>
+//   );
+// }
+
+// export default Form;
