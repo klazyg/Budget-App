600c161f1ac693e304804650516aa615be3f66a1
klazyg <kzygmunt0@gmail.com>
2023-01-23 14:22:22 +0100

    add: income subpage

---
+++ pages/income.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "../styles/income.module.scss";
+import axios from "axios";
+
+interface Transaction {
+    what: string;
+    amount: string;
+    when: string;
+}
+
+interface TransactionsProps {
+    transactions: Transaction[];
+}
+
+const Income: React.FC<TransactionsProps> = ({ transactions }) => {
+    return (
+        <div className={styles.position}>
+            <div className={styles.border}>
+                <div className={styles.text}>
+                    <div className={styles.title}>
+                        All Income
+                    </div>
+                </div>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th className={styles.what}>What</th>
+                            <th className={styles.amount}>Amount</th>
+                            <th className={styles.when}>When</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {transactions.map((transaction, index) => (
+                            <tr key={index} className={styles.tr}>
+                                <td>{transaction.what}</td>
+                                <td>{transaction.amount}</td>
+                                <td>{transaction.when}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export async function getServerSideProps() {
+    const apiResponse = await axios.get("http:localhost:3000/api/transactions");
+    const transactions = apiResponse.data.transactions.filter(transaction => transaction.type === "income");
+    return {
+        props: {
+            transactions: transactions,
+        },
+    };
+}
+
+export default Income;
