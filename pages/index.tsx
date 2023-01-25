e4abf76072b4e08f7817325ab140f813f7fb56d4
klazyg <kzygmunt0@gmail.com>
2023-01-25 10:15:51 +0100

    add: props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import TotalCosts from "../components/TotalCosts/TotalCosts";
 import Transaction from "../components/Transaction/Transaction";
 import Form from "../components/Form/Form";
-import Spending from '../components/Spending/Spending';
 import BiggestExpenses from '../components/BiggestExpenses/BiggestExpenses';
 import styles from '../styles/Home.module.scss'
 import RevenueChart from '../components/RevenueChart/RevenueChart';
 import '/styles/main.module.scss';
 import axios from 'axios';
 
-const Home: React.FC = ({ transactionsData }) => {
+interface HomeProps {
+  transactionsData: Transaction[];
+}
+
+const Home: React.FC<HomeProps> = ({ transactionsData }) => {
   const [transactions, setTransactions] = useState(transactionsData);
 
   const onSubmitTransaction = (transaction) => {
@@ -59,12 +62,10 @@ const Home: React.FC = ({ transactionsData }) => {
         <RevenueChart />
         <div className={styles.gridItem}>
           <Transaction transactions={transactions} />
-          <BiggestExpenses
-            sortedTransactions={sortedCategoryTotals} />
+          <BiggestExpenses sortedTransactions={sortedCategoryTotals} />
         </div>
       </div>
       <div className={styles.rightWidth}>
-        {/* <Spending /> */}
         <Form onSubmitTransaction={onSubmitTransaction} />
       </div>
     </div>
