b176aad573c1e2e99d44c46dba61265e41c5e339
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-03-06 14:53:11 +0100

    correct: styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import Link from 'next/link';
 import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
   return (
-    <div className={styles.section}>
+    <header className={styles.section}>
       <Link href="/">
         <h1 className={styles.title}>Home Budget</h1>
       </Link>
-    </div>
+    </header>
   );
 };
 
