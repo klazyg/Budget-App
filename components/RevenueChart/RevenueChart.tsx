f4c895855c5fbc30767b8cea6aea10d8204d5945
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-03-06 14:56:58 +0100

    add: data to chart

--- components/RevenueChart/RevenueChart.tsx
+++ components/RevenueChart/RevenueChart.tsx
@@ -1,117 +1,41 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from "./RevenueChart.module.scss";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ITransaction } from '../../pages';
 
-const data = [
-  {
-    month: 'Jan',
-    uv: 4000,
-    Income: 2400,
-    amt: 2400,
-  },
-  {
-    month: 'Feb',
-    uv: 3000,
-    Income: 1398,
-    amt: 2210,
-  },
-  {
-    month: 'Mar',
-    uv: 2000,
-    Income: 9800,
-    amt: 2290,
-  },
-  {
-    month: 'Apr',
-    uv: 2780,
-    Income: 3908,
-    amt: 2000,
-  },
-  {
-    month: 'May',
-    uv: 1890,
-    Income: 4800,
-    amt: 2181,
-  },
-  {
-    month: 'June',
-    uv: 2390,
-    Income: 3800,
-    amt: 2500,
-  },
-  {
-    month: 'July',
-    uv: 3490,
-    Income: 4300,
-    amt: 2100,
-  },
-  {
-    month: 'Aug',
-    uv: 2390,
-    Income: 3800,
-    amt: 2500,
-  },
-  {
-    month: 'Sep',
-    uv: 2780,
-    Income: 3908,
-    amt: 2000,
-  },
-  {
-    month: 'Oct',
-    uv: 4000,
-    Income: 2400,
-    amt: 2400,
-  },
-  {
-    month: 'Nov',
-    uv: 2780,
-    Income: 3908,
-    amt: 2000,
-  },
-  {
-    month: 'Dec',
-    uv: 3490,
-    Income: 4300,
-    amt: 2100,
-  },
-];
+type Props = {
+  transactions: ITransaction[];
+}
 
+const RevenueChart: React.FC<Props> = ({ transactions }) => {
+  const [chartData, setChartData] = useState([]);
 
-const RevenueChart: React.FC = () => {
-  const [chartData, setChartData] = useState(data);
+  useEffect(() => {
+    const sortedData = transactions.sort((a, b) => new Date(a.when).getTime() - new Date(b.when).getTime());
+    const groupedData = sortedData.reduce((acc, curr) => {
+      const month = new Date(curr.when).toLocaleString('en-US', { month: 'short' });
+      const existingMonth = acc.find(item => item.month === month);
 
-  const addToChart = (data) => {
-    setChartData([...chartData, data]);
-  }
+      if (existingMonth) {
+        existingMonth.Income += curr.amount;
+      } else {
+        acc.push({
+          month,
+          Income: curr.amount,
+        });
+      }
 
-  const handleChange = (month, income) => {
-    setChartData((prevState) => {
-      return prevState.map((item) => {
-        if (item.month === month) {
-          return {
-            ...item,
-            Income: income,
-          };
-        } else {
-          return item;
-        }
-      });
-    });
-  }
+      return acc;
+    }, []);
+
+    setChartData(groupedData);
+  }, [transactions]);
+
   return (
     <div className={styles.border}>
       <div className={styles.text}>
         <div className={styles.title}>
           {new Date().getFullYear()} Revenue Chart
-        </div>
-        <div className={styles.viewMore} onClick={() => addToChart({
-          month: 'Dec',
-          uv: 34,
-          Income: 1000,
-          amt: 21,
-        },)}>
-          view more
         </div>
       </div>
       <ResponsiveContainer width="100%" height="80%" className={styles.chart}>
